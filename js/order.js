e01efa454118444217464deba7b90143135cd563
Joe <root@localhost>
2019-11-22 16:50:36 +0000

    Added order stuff

--- js/order.js
+++ js/order.js
@@ -1,4 +1,34 @@
+var OrderState =
+{
+    COMPLETE: 1,
+    COOKING: 2,
+    PLATING: 3,
+    NOT_STARTED: 4
+}
+
+var EggType =
+{
+    FRIED: 1,
+    OMELETTE: 2,
+    SCRAMBLED: 3
+}
+
+var Toppings =
+{
+    LETTUCE: 1,
+    RED_ONION: 2,
+    PEPPERS: 3,
+    TOMATOES: 4,
+    SLICED_BREAD: 5,
+    KETCHUP: 6
+}
+
 class Order
 {
-    
+    constructor(customer, type, toppings)
+    {
+        this.customer = customer
+        this.type = type
+        this.toppings = toppings
+    }
 }
